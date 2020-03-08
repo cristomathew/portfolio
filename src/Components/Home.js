9cb54dbc8801348d14c200783971d6d4ed066117
overlord839 <cristomathew7@gmail.com>
2020-03-08 12:36:56 +0530

    7

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,6 +3,6 @@ import React from 'react'
 export const Home = () => (
     <div>
         <h2>Hi My Name is Cristo</h2>
-        <p>Its a pleasure meeting you guys please feel free to check up my website</p>
+        <p>Its a pleasure meeting you guys please feel free to check up my website//</p>
     </div>
 )
