ba6d294d049e556db67bb68fa71401ff266fa2d7
overlord839 <cristomathew7@gmail.com>
2020-03-08 11:09:08 +0530

    Updates

---
+++ precache-manifest.b628359c42937ae386869e50ff433555.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "240c71c045a86f7f30cfd16cacbfad60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4beeea94c9aa3fd90ecc",
+    "url": "/static/css/main.e256aee0.chunk.css"
+  },
+  {
+    "revision": "a86d75881edc5f0c87f1",
+    "url": "/static/js/2.af700454.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.af700454.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4beeea94c9aa3fd90ecc",
+    "url": "/static/js/main.ad04c478.chunk.js"
+  },
+  {
+    "revision": "a27870e5f03a3425f6aa",
+    "url": "/static/js/runtime-main.c70048c8.js"
+  }
+]);
