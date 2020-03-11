d418481e66bb52413905ae1f6f67f53481a6654f
overlord839 <cristomathew7@gmail.com>
2020-03-11 13:26:03 +0530

    new

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import Skills from './Skills';
+const SKILLS = [
+    {type: "JavaScript", level: 84},
+    {type: "React.js", level: 87},
+    {type: "Django", level: 90},
+    {type: "Python", level: 85},
+    {type: "Ionic", level: 70},
+    {type: "HTML", level: 99},
+    {type: "CSS", level: 90}
+  ];
+  
 export const About = () => (
     <div>
-        <h2>This is About Page</h2>
-        <p>Its a pleasure meeting you guys please feel free to check up my website</p>
+        <h2>About Me</h2>
+        <Skills skills={SKILLS} width="89%" />
     </div>
 )
---
+++ src/Components/Skills.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import '../Styling/skills.css';
+
+
+export default class Skills extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { collapsed: true };       
+    }
+    
+    componentDidMount() {
+      setTimeout(() => {
+        this.setState({ collapsed: false })
+      }, 800);
+    }
+  
+    render() {
+      const { collapsed } = this.state;
+      const { hue, saturation, skills } = this.props;
+  
+      return(  
+        <div id="app" className={`container ${collapsed ? 'collapsed' : ''}`}>
+          <h1>Skills</h1><hr/>
+          <ul className="skills">
+            {skills.map((skill, index) => 
+              <li
+                key={skill.type}
+                style={{ width: `${skill.level}%`, backgroundColor: `hsl(${hue}, ${saturation}%, ${100 / (index + 3.5) }%)` }}
+              >
+                <p>{skill.type}<span>{skill.level}</span></p>
+              </li>
+            )}
+          </ul>
+        </div>
+      )
+    }
+  }
+  
