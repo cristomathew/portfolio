7e23d6a3381b79aa867e56e8db6ae919e5215bea
overlord839 <cristomathew7@gmail.com>
2020-03-13 19:12:37 +0530

    projects

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,14 +4,14 @@ import '../Styling/css/style.css'
 
 export default function Card(props) {
     return (
-        <div className="card">
-            <div className="card-image" />
-            <div className="card-text">
+        <div className="infocard">
+            <div className="infocard-image" />
+            <div className="infocard-text">
                 <span className="date">{props.date}</span>
                 <h2>{props.head}</h2>
                 <p>{props.children}</p>
             </div>
-            <div className="card-stats notfound notfound-social">
+            <div className="infocard-stats notfound notfound-social">
                 <a href="https://www.instagram.com/cris00707/"><i class="fa fa-instagram"></i></a>
                 <a href="https://www.linkedin.com/in/cristo-mathew-6b145a160/"><i class="fa fa-linkedin"></i></a>
                 <a href="https://www.github.com/cristomathew"><i class="fa fa-github"></i></a>
